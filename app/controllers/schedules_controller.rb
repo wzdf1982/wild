57406fbc71af9232bb7284aa180d73067d7f021a
Dingjun Zhou <dingjun.zhou@gmail.com>
2013-09-29 02:34:28 +0000

    add max column span

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,11 +1,11 @@
 class SchedulesController < ApplicationController
   def index
     start_times = Activity.all.group(:start_time).select(:start_time)
-
-    puts start_times
     @groups = []
     start_times.each do |time|
       @groups << Activity.where(start_time: time.start_time).order('position')
     end
+
+    @max_column_span = Activity.all.select('column_span').max.column_span
   end
 end
