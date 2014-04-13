de293a2d5d313a77d8731315f8c6a457cffea4dd
Dingjun Zhou <dingjun.zhou@gmail.com>
2014-04-13 14:20:01 +0000

    remove mobile css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,38 @@
 //
 //= require jquery-1.8.3.min
 //= require jquery_ujs
+//= require jquery.mobile-1.2.1.min
+//= require vote
 
+var ready = function(){
+  $("#UserCode").bind("focus", function(){
+    var iUserCode=$("#UserCode").val();
+    if(iUserCode.toUpperCase()=='Ericsson Corporate ID'.toUpperCase())
+      {
+        $("#UserCode").val("") ;
+      }
+      var o=document.getElementById("UserCode");
+      o.style.color='#666666';
+  })
+  $("#UserCode").bind("blur", function(){
+    var iUserCode=$("#UserCode").val();
+    if(iUserCode=='')
+      {
+        $("#UserCode").val('Ericsson Corporate ID');
+      }
+      var o=document.getElementById("UserCode");
+      o.style.color='#c8c8c8';
+  })
+}
 
+function refreshPage() {
+  $.mobile.changePage(
+    window.location.href,
+    {
+      allowSamePageTransition : true,
+      transition              : 'none',
+      showLoadMsg             : true,
+      reloadPage              : true
+    }
+  );
+}
